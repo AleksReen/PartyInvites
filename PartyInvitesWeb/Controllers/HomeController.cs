c063b0662f036238ebf873447443a3a982115866
DESKTOP-8ICK793\aleks <aleksandr.reentovich@tut.by>
2018-05-19 22:09:36 +0300

    Simple-Demo MVC app

---
+++ PartyInvitesWeb/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using PartyInvitesWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvitesWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var date = DateTime.Now.Hour;
+            ViewBag.Greeting = date > 12 ? "PM now" : "AM now";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guest);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
